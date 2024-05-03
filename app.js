f2687a6960f5657484d145b810b50ce11520202c
Akash Kumar Tiwary <btech10172.22@bitmesra.ac.in>
2024-05-03 18:19:02 +0530

    Added Scroll reveal

--- app.js
+++ app.js
@@ -25,3 +25,27 @@ container.addEventListener("click", () => {
     sun.classList.toggle("sun-anim");
     moon.classList.toggle("moon-anim");
 })
+
+ScrollReveal({
+    reset : true,
+    distance : '60px',
+    duration : 2500,
+    delay : 400
+});
+ScrollReveal().reveal('.my-image', { delay: 200, origin: 'left' });
+// ScrollReveal().reveal('.intro-details', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.title', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.contents', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.h1-code', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.clubs', { delay: 200, origin: 'left' });
+// ScrollReveal().reveal('.skills-intro', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.li-skills', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.sites', { delay: 200, origin: 'left' });
+ScrollReveal().reveal('.card-container', { delay: 200, origin: 'left' });
+
+var typed = new Typed(".auto-type", {
+    strings : ["Web Developer", "Sport Programmer", "Electrical Engineer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+})
